cf549cca82ce5123bc39fe7b0e76c98cf26b6f1e
aymather <aymather@gmail.com>
2020-08-03 18:50:51 -0700

    Initial Commit

---
+++ iALT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iALT
+//
+//  Created by Alec Mather on 8/3/20.
+//  Copyright © 2020 Alec Mather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
